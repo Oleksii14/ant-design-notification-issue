7300e43c5cba2e2aa6d5a73050cb54b0fb8137f1
oleksiishaposhnikov <oshap1044@gmail.com>
2022-06-03 19:39:20 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { notification } from 'ant-design-vue'
+
+notification.open({
+  message: 'test',
+  description: 'description',
+  duration: 10000,
+})
+</script>
+
+<template>
+  Test
+</template>
